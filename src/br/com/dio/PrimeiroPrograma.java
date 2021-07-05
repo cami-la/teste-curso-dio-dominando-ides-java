c2292115bb28501435a4e3664f2e2f8b708516df
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-07-04 22:43:12 -0300

    criação calculadora

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,11 +7,11 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		Gato gato = new Gato();
 		Livro livro = new Livro();
-		
+
 		System.out.println(gato);
 		System.out.println(livro);
-	}
 
+	}
 }
 
 class Livro {
---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,48 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a, b;
+		
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextInt();
+		
+		int soma = soma(a,b);
+		int subtracao = subtracao(a,b);
+		int multiplicacao = multiplicacao(a,b);
+		int divisao = divisao(a,b);
+		
+		System.out.println("soma " + soma);
+		System.out.println("sub " + subtracao);
+		System.out.println("mult " + multiplicacao);
+		System.out.println("div " + divisao);
+		
+	}
+	
+	public static int soma(int a, int b) {
+		return a + b;
+	}
+	
+	public static int subtracao(int a, int b) {
+		return a - b;
+	}
+	
+	public static int divisao(int a, int b) {
+		return a / b;
+	}
+	
+	public static int multiplicacao(int a, int b) {
+		return a * b;
+	}
+	
+	
+	
+
+}
